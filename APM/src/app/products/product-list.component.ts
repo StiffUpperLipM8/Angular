8a1bf58f5839b6452feedb583e1131d9380c7dce
Stanislav_Sydorenko <Stanislav_Sydorenko@epam.com>
2018-06-07 16:04:50 +0300

    added comments

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,4 +1,3 @@
-
 import {Component, OnInit} from '@angular/core';
 import {IProduct} from "./product";
 
@@ -7,67 +6,66 @@ import {IProduct} from "./product";
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit{
+export class ProductListComponent implements OnInit {
   pageTitle: string = 'Product list';
-    imageWidth: number = 50;
-    imageMargin: number = 2;
-    showImage: boolean = false;
-    private _listFilter: string;
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
+  private _listFilter: string;
 
-    filteredProducts: IProduct[];
-    products: IProduct[] = [ {
-      "productId": 1,
-      "productName": "Leaf Rake",
-      "productCode": "GDN-0011",
-      "releaseDate": "March 19, 2016",
-      "description": "Leaf rake with 48-inch wooden handle.",
-      "price": 19.95,
-      "starRating": 3.2,
-      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-    },
-      {
-        "productId": 2,
-        "productName": "Garden Cart",
-        "productCode": "GDN-0023",
-        "releaseDate": "March 18, 2016",
-        "description": "15 gallon capacity rolling garden cart",
-        "price": 32.99,
-        "starRating": 4.2,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-      }];
+  filteredProducts: IProduct[];
+  products: IProduct[] = [{
+    "productId": 1,
+    "productName": "Leaf Rake",
+    "productCode": "GDN-0011",
+    "releaseDate": "March 19, 2016",
+    "description": "Leaf rake with 48-inch wooden handle.",
+    "price": 19.95,
+    "starRating": 3.2,
+    "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+  },
+    {
+      "productId": 2,
+      "productName": "Garden Cart",
+      "productCode": "GDN-0023",
+      "releaseDate": "March 18, 2016",
+      "description": "15 gallon capacity rolling garden cart",
+      "price": 32.99,
+      "starRating": 4.2,
+      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+    }];
 
   constructor() {
-     this.filteredProducts = this.products;
+    this.filteredProducts = this.products;
   }
-
 
   get listFilter(): string {
     return this._listFilter;
   }
 
-  // every time the listValue changes (e.g. in html), the setter is invoked, so we can add a filtering logic here
+  // every time the istFilter changes (e.g. in html), the setter is invoked, so we can add a filtering logic here
   set listFilter(value: string) {
     this._listFilter = value;
     this.filteredProducts = this._listFilter ? this.performFilter(this._listFilter) : this.products;
   }
 
   toggleImage(): void {
-      this.showImage = !this.showImage;
-    }
+    this.showImage = !this.showImage;
+  }
 
-    getButtonText(): string {
-       return (this.showImage) ? "Hide image" : "Show image";
-    }
+  getButtonText(): string {
+    return (this.showImage) ? "Hide image" : "Show image";
+  }
 
-    ngOnInit(): void {
-       console.log("On init");
-    }
+  ngOnInit(): void {
+    console.log("On init");
+  }
 
-    performFilter(filterBy: string): IProduct[] {
-       filterBy = filterBy.toLowerCase();
-       return this.products.filter((product: IProduct) =>
-              product.productName.toLowerCase().indexOf(filterBy) !== -1);
-    }
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLowerCase().indexOf(filterBy) !== -1);
+  }
 
 
 }
