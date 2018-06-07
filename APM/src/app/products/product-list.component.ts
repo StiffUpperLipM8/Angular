a28864be9017043593906264148e4439f9f3db3d
Stanislav_Sydorenko <Stanislav_Sydorenko@epam.com>
2018-06-07 13:54:11 +0300

    added interface, css file

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,18 +1,19 @@
 
 import {Component} from '@angular/core';
+import {IProduct} from "./product";
 
 @Component({
   selector: 'pm-products',
-  templateUrl: './product-list.component.html'
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
     pageTitle: string = 'Product list';
     imageWidth: number = 50;
-
     imageMargin: number = 2;
     showImage: boolean = false;
     listFilter: string = "cart";
-    products: any[] = [ {
+    products: IProduct[] = [ {
       "productId": 1,
       "productName": "Leaf Rake",
       "productCode": "GDN-0011",
